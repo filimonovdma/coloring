f9cb19d18bf7deeace999bb8e6725c45c656ad69
filimda <filimda@yandex.ru>
2023-03-23 22:31:37 +0300

    добавлены основные категории

--- coloring/models.py
+++ coloring/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
+from modelcluster.tags import ClusterTaggableManager
+from taggit.models import Tag as TaggitTag
+from taggit.models import TaggedItemBase
+from wagtail.admin.edit_handlers import (
+FieldPanel,
+FieldRowPanel,
+InlinePanel,
+MultiFieldPanel,
+PageChooserPanel,
+StreamFieldPanel,
+)
 from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
-from taggit.models import Tag as TaggitTag
-
 class HomePage(Page):
     description = models.CharField(max_length=255, blank=True,)
     content_panels = Page.content_panels + [FieldPanel("description", classname="full")]
@@ -18,10 +28,30 @@ class ColoringPage(Page):
         on_delete=models.SET_NULL,
         related_name="+",
         )
+    tags = ClusterTaggableManager(through="ColoringPageTag", blank=True)
+
     content_panels = Page.content_panels + [
-        ImageChooserPanel("header_image"),
+        ImageChooserPanel('header_image'),
+        FieldPanel('body'),
+        FieldPanel('tags'),
+        InlinePanel("categories", label="category"),
         ]
-    
+class PostPageColoringCategory(models.Model):
+    page = ParentalKey(
+        "ColoringPage",
+        on_delete=models.CASCADE,
+        related_name="categories"
+)
+    coloring_category = models.ForeignKey(
+        'ColoringCategory',
+        on_delete=models.CASCADE,
+        related_name='colorin_pages'
+    )
+    panels = [
+        SnippetChooserPanel('coloring_category'),
+    ]
+    class Meta:
+        unique_together =('page', 'coloring_category')
 @register_snippet
 class ColoringCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -35,8 +65,11 @@ class ColoringCategory(models.Model):
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
-
+class ColoringPageTag(TaggedItemBase):
+    content_object = ParentalKey("ColoringPage", related_name="coloring_page_tags")
 @register_snippet
-class ColoringTag(TaggitTag):
+class Tag(TaggitTag):
     class Meta:
         proxy = True
+
+######
